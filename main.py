092c5935033b811a95aa1a14f19533ab359fc734
imsoumya18 <soumyadeep184@gmail.com>
2021-11-17 23:34:39 +0530

    chng

--- main.py
+++ main.py
@@ -17,16 +17,36 @@ async def on_message(message):
         with open('backup.json', 'r') as f:
             data = json.load(f)
             curr = data['password']
-            print(f'Current password: {curr}')
         with open('backup.json', 'w') as f:
             new = message.content.split()[1]
             data['password'] = new
-            print(f'New password: {new}')
             json.dump(data, f)
-    if message.content.lower() == 'pswd':
-        member = message.author
-        role = get(member.guild.roles, name="verified")
-        await member.remove_roles(role)
+            embedparam = discord.Embed(title='Password updated', color=0x0addd7)
+            embedparam.add_field(name='New Password:', value=new)
+            await message.channel.send(embed=embedparam)
+            await message.delete()
+    if message.content.startswith('!pswd'):
+        pswd = message.content.split()[1]
+        with open('backup.json', 'r') as f:
+            data = json.load(f)
+        if pswd == data['password']:
+            member = message.author
+            role = get(member.guild.roles, name="verified")
+            role2 = get(member.guild.roles, name="unverified")
+            await member.add_roles(role)
+            await member.remove_roles(role2)
+            embedparam = discord.Embed(title='You are verified now', color=0x0add1f)
+            await message.channel.send(embed=embedparam)
+            await message.delete()
+        else:
+            member = message.author
+            role = get(member.guild.roles, name="unverified")
+            role2 = get(member.guild.roles, name="verified")
+            await member.add_roles(role)
+            await member.remove_roles(role2)
+            embedparam = discord.Embed(title='Wrong password!!', color=0xff1e00)
+            await message.channel.send(embed=embedparam)
+            await message.delete()
 
 
 bot.run(TOKEN)
